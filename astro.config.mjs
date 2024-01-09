b13f69d460d209bfc88b598272aadb8970b66821
david.zeplin <zeplindavid@gmail.com>
2024-01-09 15:44:42 +0100

    modified link in hero section on landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
+  viewTransitions: true,
   integrations: [tailwind(), compress()],
+  output: "server",
+  adapter: netlify()
 });
